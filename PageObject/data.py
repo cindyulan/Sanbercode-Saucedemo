68d740733298c6bb0b67508e23fe9228a2bc68ef
cindyulan <cindyulanp03@gmail.com>
2023-05-30 23:00:42 +0700

    page object module

---
+++ PageObject/data.py
@@ -0,0 +1,9 @@
+class inputan():
+    invalidCredential = "Epic sadface: Username and password do not match any user in this service"
+    requiredUsn = "Epic sadface: Username is required"
+    requiredPw = "Epic sadface: Password is required"
+    username = "standard_user"
+    password = "secret_sauce"
+    successLogin ="Products"
+    wrongUsername = "abcdef"
+    wrongPassword = "abc123"
---
+++ PageObject/locator.py
@@ -0,0 +1,12 @@
+class elem():
+    loginButton = "login-button"
+    username = "user-name"
+    password = "password"
+    invCreden = "[data-test=error]"
+    produk = "title"
+    addProduk = "[data-test=add-to-cart-sauce-labs-backpack]"
+    cart = "shopping_cart_container"
+
+class link():
+    baseUrl = "https://www.saucedemo.com/"
+    cart = "https://www.saucedemo.com/cart.html"
